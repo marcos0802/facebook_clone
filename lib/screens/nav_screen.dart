93024f42ee083d7f0fb1b062e0cf5accfe4462df
root <root@localhost.localdomain>
2021-09-18 23:26:04 +0100

    feat : set web responsivness

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_clone/data/data.dart';
 import 'package:facebook_clone/screens/screens.dart';
 import 'package:facebook_clone/widgets/widgets.dart';
 import 'package:flutter/cupertino.dart';
@@ -34,21 +35,33 @@ class _NavScreenState extends State<NavScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return DefaultTabController(
         length: _icons.length,
         child: Scaffold(
+          appBar: Responsive.isDesktop(context)
+              ? PreferredSize(
+                  preferredSize: Size(screenSize.width, 100.0),
+                  child: CustomAppBar(
+                      currentUser: currentUser,
+                      icons: _icons,
+                      selectedIndex: _selectedIndex,
+                      onTap: (index) => setState(() => _selectedIndex = index)))
+              : null,
           body: IndexedStack(
             index: _selectedIndex,
             children: _screens,
           ),
-          bottomNavigationBar: Padding(
-            padding: const EdgeInsets.only(bottom: 10.0),
-            child: CustomTabBar(
-              icons: _icons,
-              selectedIndex: _selectedIndex,
-              onTap: (index) => setState(() => _selectedIndex = index),
-            ),
-          ),
+          bottomNavigationBar: !Responsive.isDesktop(context)
+              ? Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: CustomTabBar(
+                    icons: _icons,
+                    selectedIndex: _selectedIndex,
+                    onTap: (index) => setState(() => _selectedIndex = index),
+                  ),
+                )
+              : const SizedBox.shrink(),
         ));
   }
 }
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,66 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:facebook_clone/models/user_model.dart';
+import 'package:facebook_clone/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar(
+      {Key? key,
+      required this.currentUser,
+      required this.icons,
+      required this.selectedIndex,
+      required this.onTap})
+      : super(key: key);
+
+  final User currentUser;
+  final List<IconData> icons;
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      height: 65.0,
+      decoration: const BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(color: Colors.black12, offset: Offset(0, 2), blurRadius: 4.0)
+      ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(
+            child: Text(
+              'facebook',
+              style: TextStyle(
+                  color: Palette.facebookBlue,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.2),
+            ),
+          ),
+          SizedBox(
+            width: 600.0,
+            child: CustomTabBar(
+              icons: icons,
+              selectedIndex: selectedIndex,
+              onTap: onTap,
+            ),
+          ),
+          Expanded(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              UserCard(user: currentUser),
+              const SizedBox(
+                width: 12.0,
+              ),
+              CircleButton(icon: Icons.search, iconSize: 30.0, onPressed: (){}),
+              CircleButton(icon: FontAwesomeIcons.facebookMessenger, iconSize: 30.0, onPressed: (){})
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
